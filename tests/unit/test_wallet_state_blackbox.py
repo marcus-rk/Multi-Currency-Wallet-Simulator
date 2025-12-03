5f260487ffadce5270294e6a0e8f3286a0643649
marcus_rk <makj0005@stud.kea.dk>
2025-12-03 14:18:09 +0100

    Remove placeholder test functions from wallet exchange, state, and withdraw blackbox tests

--- tests/unit/test_wallet_state_blackbox.py
+++ tests/unit/test_wallet_state_blackbox.py
@@ -1,15 +1,3 @@
 """
 Blackbox tests for wallet state transitions.
 """
-
-def test_freeze_wallet():
-    # TODO: Implement test for freezing wallet
-    pass
-
-def test_unfreeze_wallet():
-    # TODO: Implement test for unfreezing wallet
-    pass
-
-def test_close_wallet():
-    # TODO: Implement test for closing wallet
-    pass
--- tests/unit/test_wallet_withdraw_blackbox.py
+++ tests/unit/test_wallet_withdraw_blackbox.py
@@ -1,15 +1,3 @@
 """
 Blackbox tests for wallet withdraw logic.
 """
-
-def test_withdraw_success():
-    # TODO: Implement test for successful withdrawal
-    pass
-
-def test_withdraw_insufficient_funds():
-    # TODO: Implement test for insufficient funds
-    pass
-
-def test_withdraw_invalid_amount():
-    # TODO: Implement test for invalid withdrawal amount
-    pass
